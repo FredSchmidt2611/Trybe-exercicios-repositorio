ecd3af89e1d2cd583ba36622d5d21d68888cc7ea
FredSchmidt2611 <fredsp2008@gmail.com>
2022-04-10 14:48:39 -0300

    exercicios do dia 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/somaValoresArray.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/somaValoresArray.js
@@ -5,3 +5,30 @@ for (let index=0; index<numbers.length; index +=1) {
 soma += numbers[index]
 }
 console.log(soma);
+
+
+let numbers1=[5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let soma1=0;
+let media;
+for (let index=0; index<numbers1.length; index +=1) {
+    
+soma1 += numbers1[index]
+}
+media= soma1/numbers1.length;
+console.log(media);
+
+let numbers2=[5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let soma2=0;
+let media1;
+for (let index=0; index<numbers2.length; index +=1) {
+    
+soma2 += numbers2[index]
+}
+media1= soma2/numbers2.length;
+
+if (media1>20){
+    console.log("valor maior que 20");
+}else{
+    console.log("valor menor ou igual a 20");
+}
+
