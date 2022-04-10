e4cf01642a6a08cef21dd7b3ca7c0ddbe1cf706d
FredSchmidt2611 <fredsp2008@gmail.com>
2022-04-10 14:28:40 -0300

    exercicios com for e for/of

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -3,3 +3,24 @@ for(let index=0; index<numbers.length;index +=1){
     console.log(numbers[index]);
 }
 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices=menu[1]
+console.log(menuServices);
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio')
+console.log(indexOfPortfolio);
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu2.push('Contato')
+console.log(menu2);
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index=0;index<groceryList.length; index += 1){
+    console.log(groceryList[index]);
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+    console.log(name);
+}
