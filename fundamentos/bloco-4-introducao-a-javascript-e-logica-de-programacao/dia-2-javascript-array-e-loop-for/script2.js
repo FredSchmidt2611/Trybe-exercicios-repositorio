8291f50e5dc383cf62704a17a0dba1620da652af
FredSchmidt2611 <fredsp2008@gmail.com>
2022-04-10 11:59:22 -0300

    exercicios ou

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script2.js
@@ -15,3 +15,10 @@ if(currentHour>=22){
     message="Horario de tirar uma soneca"
 }
 console.log(message);
+
+let weekday="quarta-feira";
+if(weekday==="segunda-feira" || weekday==="terça-feira" || weekday==="quarta-feira" || weekday==="quinta-feira" || weekday==="sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}else{
+    console.log("FINALMENTE, descanso merecido UwU");
+}
