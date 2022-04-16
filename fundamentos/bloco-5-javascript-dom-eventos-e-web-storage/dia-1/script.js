55770efa039bd30eee667e66d7354b1c31e59d9d
FredSchmidt2611 <fredsp2008@gmail.com>
2022-04-16 11:00:58 -0300

    exercicios 5.1 course

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText= "Matrix"
+document.getElementById("second-paragraph").innerText= "Melhor filme de ficcao"
+document.getElementById("subtitle").innerText= "Neo era o protagonista"
