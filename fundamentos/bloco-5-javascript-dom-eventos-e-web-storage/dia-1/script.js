0ac38a64717619bc3fcd7b28a2572f90fe3cd963
FredSchmidt2611 <fredsp2008@gmail.com>
2022-04-16 21:44:50 -0300

    exercicios do dia 5.1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1/script.js
@@ -1,3 +1,5 @@
 document.getElementById("page-title").innerText= "Matrix"
 document.getElementById("second-paragraph").innerText= "Melhor filme de ficcao"
 document.getElementById("subtitle").innerText= "Neo era o protagonista"
+document.getElementsByClassName("parag1")[1].innerText="segundo paragrafo modificado"
+document.getElementsByTagName("subtitle")[0].innerText
