9496f4e7982d88249e64da694fa9900860479ca9
FredSchmidt2611 <fredsp2008@gmail.com>
2022-05-20 10:04:33 -0300

    exercicios fixacao

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_5/exerciciofixaçao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_5/exerciciofixaçao.js
@@ -14,5 +14,24 @@ const nameJobs = {...user, ...jobInfos}
 
 const { name, age, nationality, profession, squad, squadInitials} = nameJobs
 
-console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+//console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
 
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+const arrays = [ola] = saudacoes
+
+//console.log(ola);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+const newArray = [comida, animal, bebida];
+const recombinacaoArray = [animal, bebida, comida] = newArray
+
+//console.log(recombinacaoArray); // arroz gato água
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[,,, ...numerosPares] = numerosPares
+
+console.log(numerosPares); // [6, 8, 10, 12];
