c9074bb881501f867a9b781518a71ae793f0c574
FredSchmidt2611 <fredsp2008@gmail.com>
2022-05-21 14:48:45 -0300

    exercicios 1 ao 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_2/exercicio1.js
@@ -0,0 +1,118 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+
+  //1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+  
+  function authorBornIn1947() {
+    return books.find((element) => element.author.birthYear === 1947)
+  }
+
+  //console.log(authorBornIn1947());
+
+ // 2 - Retorne o nome do livro de menor nome.
+
+ function smallerName() {
+  let nameBook;
+   books.forEach ( (element) => {
+     if (!nameBook || element.name.length < nameBook.length){
+       nameBook = element.name
+     }
+   } )
+  
+  return nameBook;
+}
+
+//console.log(smallerName());
+
+function getNamedBook() {
+  return books.find( (book) => book.name.length === 26).name
+}
+
+//console.log(getNamedBook());
+
+//4 - Ordene os livros por data de lançamento em ordem decrescente.
+
+function booksOrderedByReleaseYearDesc() {
+  return books.sort( (a, b) => b.releaseYear - a.releaseYear )
+}
+
+//console.log(booksOrderedByReleaseYearDesc());
+
+//5 - Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+
+function everyoneWasBornOnSecXX() {
+  return books.every( (element) => element.author.birthYear <= 2000);
+}
+
+//console.log(everyoneWasBornOnSecXX());
+
+//6 - Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((element) => element.releaseYear>=1980 && element.releaseYear <= 1989 )
+}
+
+//console.log(someBookWasReleaseOnThe80s());
+
