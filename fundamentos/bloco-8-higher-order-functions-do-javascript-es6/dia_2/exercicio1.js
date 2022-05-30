2982b70fc41852f79fd3512090dd73192ffd49ef
FredSchmidt2611 <fredsp2008@gmail.com>
2022-05-29 22:06:14 -0300

    exercicios 2, 3, 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_2/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_2/exercicio1.js
@@ -62,21 +62,37 @@ const books = [
   ];
 
  //  1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
- /* const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-]; */
 
 function formatedBookNames(array) {
   return array.map((element) => `${element.name} - ${element.genre} - ${element.author.name}`);
 }
 
-console.log(formatedBookNames(books));
+// console.log(formatedBookNames(books));
 
+/* 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+ */
+
+function nameAndAge() {
+  return books.map((element) =>({
+    age: element.releaseYear - element.author.birthYear,
+    author: element.author.name,
+  })).sort((a, b) => a.age - b.age);
+}
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+function fantasyOrScienceFiction() {
+  return books.filter((element) => element.genre === 'Fantasia' || element.genre === 'Ficção Científica');
+}
+
+// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+
+function oldBooksOrdered() {
+  const currentYear = 2022
+  return books.filter((element)  => element.releaseYear < currentYear - 60).sort((a, b) =>  a.releaseYear - b.releaseYear)
+}
+
+console.log(oldBooksOrdered());
   //1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
   
   function authorBornIn1947() {
