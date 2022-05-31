84e6f2979f620149df5c6aedca2a722f3f6f2658
FredSchmidt2611 <fredsp2008@gmail.com>
2022-05-31 15:12:18 -0300

    teste

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_4/exercicio5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia_4/exercicio5.js
@@ -5,13 +5,18 @@ const names = [
   ];
   
   
+ // 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 
+ const letters = names.splice('');
   function containsA() {
-    return names.reduce( (acc, curr) => 
+     return names.reduce( (acc, curr) => 
  acc + curr.split('').reduce( (sum, name) => {
      if (name === 'A' || name === 'a') return sum + 1;
          return sum;
-        }, 0), 0)
+        }, 0), 0)  
+  
+  /* const x = letters.reduce((acc, letter) => (letter === 'a' || letter === 'A')? acc + 1: acc + 0)
+  return x.length       */
   }
 
-  console.log(containsA());
+  console.log(letters);
