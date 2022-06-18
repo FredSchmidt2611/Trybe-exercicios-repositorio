03ee9347496bec3f43dbf660b2f933e7939351a7
FredSchmidt2611 <fredsp2008@gmail.com>
2022-06-18 16:23:16 -0300

    exercicios 1 e 2

---
+++ fundamentos/bloco-10-introducao-a-react/dia_1/exercicio/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Fred Schmidt</h1>
+                <p>Aspirante a programador Web. Aluno da Trybe.</p>
+            </div>
+        )
+    }
+}
+
+export default About;
