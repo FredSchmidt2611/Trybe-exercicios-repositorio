4156e098ad53258aaa38c948f327e8c10a78b9cc
FredSchmidt2611 <fredsp2008@gmail.com>
2022-06-25 10:32:31 -0300

    exercicio 1

---
+++ fundamentos/bloco-10-introducao-a-react/dia_2/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,13 @@
+// import staringCat from './staringCat.jpg';
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+  <main>
+    <Image source={staringCat} alt="Cute cat staring" />
+  </main>
+  );
+}
+
+export default App;
