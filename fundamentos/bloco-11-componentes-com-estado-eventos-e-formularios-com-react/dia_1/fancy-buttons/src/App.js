bed882d5ac80599ae270a2c7950a8dc2c0dff8ca
FredSchmidt2611 <fredsp2008@gmail.com>
2022-06-26 11:30:15 -0300

    exercicio fixacao

--- fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia_1/fancy-buttons/src/App.js
+++ fundamentos/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia_1/fancy-buttons/src/App.js
@@ -27,10 +27,14 @@ class App extends React.Component {
     console.log('Bolsonaro Presidente');
   }
 
+  mudaCor(numero) {
+    return numero % 2 === 0? 'green' : 'white';
+  }
   render() {
     return (
       <>
-      <button onClick={this.handleClick}>{this.state.cliques}</button>
+      <button onClick={this.handleClick} 
+      style={{backgroundColor: this.mudaCor(this.state.cliques)}}>{this.state.cliques}</button>
       <button onClick={this.handleClick2}>Botão</button>
       <button onClick={this.handleClick3}>Botão</button>
       </>
